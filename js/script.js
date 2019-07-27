f8c18721d0095c3d467f33fdee53c20bbbb5a059
Shahriar Hridoy <multi.hridoy@gmail.com>
2019-07-27 23:45:14 +0600

    added some function to script.js

--- js/script.js
+++ js/script.js
@@ -17,18 +17,34 @@ const checkedCellOfPlayerX = [];
 const checkedCellOfPlayerO = [];
 
 
-//add event listener to borad
+//add event listener to board
 board.addEventListener("click", e => {
     const cellId = e.target.getAttribute("data-cell-id");
+    // check cell id is in plyerX or plyerO
+    if (!(checkedCellOfPlayerX.includes(cellId) || checkedCellOfPlayerO.includes(cellId))) {
+        if (playerTurn === "x") {
+            e.target.innerHTML = `<i class="fa fa-close"></i>`
+            checkedCellOfPlayerX.push(cellId);
+            matchcheck(checkedCellOfPlayerX)
+            //change palyer to O
+            // playerTurn = "o";
 
+        } else if (playerTurn === "o") {
+
+        }
+    }
 })
 
-// function
-const playerClick = cellId => {
-    if (playerTurn == "x") {
-
-    } else if (playerTurn == "0") {
-
+function matchcheck(playerArr){
+    for (let k =0;k<patterns.length;k++){
+    	let hasIt = patterns[k].reduce((total,i)=> total && playerArr.includes(i) ,true);
+        if(hasIt){
+            winnerChecker(patterns[k])
+        }
     }
+    return false
 }
 
+function winnerChecker(patterns){
+    alert("win")
+}
