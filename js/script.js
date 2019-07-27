74f2c0edc77d6bbddd151e74363523488865bbfb
Shahriar Hridoy <multi.hridoy@gmail.com>
2019-07-27 23:15:31 +0600

    added basic event to board

---
+++ js/script.js
@@ -0,0 +1,17 @@
+//select board
+const board = document.querySelector(".board");
+
+//add event listener to borad
+board.addEventListener("click", e => {
+    const cellId = e.target.getAttribute("data-cell-id");
+
+})
+
+// function
+const playerClick = cellId => {
+    if (playerTurn == "x") {
+
+    } else if (playerTurn == "0") {
+
+    }
+}
